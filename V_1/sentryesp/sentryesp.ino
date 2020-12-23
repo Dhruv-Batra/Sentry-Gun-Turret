2bbfd5b99f7c10c58987e0fbeeac2c5f245777f3
Dhruv Batra <dhruvbatra2@gmail.com>
2020-12-23 18:06:57 -0500

    file organization

--- V_1/sentryesp/sentryesp.ino
+++ V_1/sentryesp/sentryesp.ino
@@ -1,30 +1,34 @@
-/*#include <Servo.h>*/
+#include <ESP32Servo.h>
 
 const int pingPin = 12; // Trigger Pin of Ultrasonic Sensor
 const int echoPin = 14; // Echo Pin of Ultrasonic Sensor
 const int RELAY_PIN = 26;  // the Arduino pin, which connects to the IN pin of relay
-const int LASER = 13;
+const int LASER = 27;
 boolean flag=false;
 int range = 5; //change number for at what minimum distance away should sentry be triggered
 
-//Servo myservo;  // create servo object to control a servo
-int pos = 0;    // variable to store the servo position
+Servo myservo;  // create servo object to control a servo
+int pos = 230;    // variable to store the servo position
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  Serial.begin(115200); // Starting Serial Terminal
+  Serial.begin(9600); // Starting Serial Terminal
   pinMode(RELAY_PIN, OUTPUT);
   pinMode (LASER, OUTPUT); 
-  //myservo.attach(27);  
+  // Allow allocation of all timers
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  myservo.setPeriodHertz(50);    // standard 50 hz servo
+  myservo.attach(13, 500, 2400); // attaches the servo on pin 13 to the servo object 
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  laseron();
-  relayon();
-  delay(1000);
-}
-  /*if (dist() <= range){
+  if (dist() <= range){
+    myservo.attach(13, 500, 2400);
+    trigger();
     laseron();
     relayon();
     flag = true;
@@ -32,12 +36,14 @@ void loop() {
     laseroff();
     relayoff();
     flag=false;
+    myservo.detach();
     delay(100);
   } else {
     flag=false;
+    myservo.detach();
     delay(100);
   }
-}*/
+}
 
 void relayon () {
   digitalWrite(RELAY_PIN, HIGH);
@@ -50,12 +56,12 @@ void relayoff () {
 }
 
 void laseron () {
-  digitalWrite(LASER, LOW); // open the laser head
+  digitalWrite(LASER, HIGH); // open the laser head
   delay(100);
 }
 
 void laseroff () {
-  digitalWrite(LASER, HIGH); // turn off the laser head
+  digitalWrite(LASER, LOW); // turn off the laser head
   delay(100);
 }
 
@@ -77,17 +83,18 @@ int dist (){
    return inches;
 }
 
-/*void trigger (){
-  for (pos = 0; pos <= 50; pos += 1) { // goes from 0 degrees to 180 degrees
+void trigger (){
+  for (pos = 180; pos <= 330; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    myservo.write(pos);    // tell servo to go to position in variable 'pos'
+    delay(15);             // waits 15ms for the servo to reach the position
   }
-  for (pos = 50; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  for (pos = 270; pos >= 260; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(0);    // tell servo to go to position in variable 'pos'
+    delay(15);             // waits 15ms for the servo to reach the position
   }
-}*/
+  myservo.detach();
+}
 
 long microsecondsToInches(long microseconds) {
    return microseconds / 74 / 2;
